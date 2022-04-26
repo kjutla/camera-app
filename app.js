dcd6b04fa9ba257884cf8ff9b191da8ed3a03fdd
kjutla <64712718+kjutla@users.noreply.github.com>
2022-04-26 16:46:19 -0700

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+//Constraints
+var constraints = { video: {facingMode: "user"}, audio: false }; 
+
+//Constants
+const cameraView = document.querySelector("#camera--view");
+const cameraOutput = document.querySelector("#camera--output");
+const cameraSensor = document.querySelector("#camera--sensor"); 
+const cameraTrigger = document.querySelector("#camera--trigeer"); 
+
+//Access device camera 
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks() [0];
+            cameraView.srcObject = stream; 
+        })
+        .catch(function(error) {
+            console.error("it do be broken", error);
+        });
+}
+
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
